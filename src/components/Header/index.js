12d9b39a60a73c91febe6163383521ff034b52a1
vsathyanagasai <vsathyanagasai@gmail.com>
2023-04-19 08:47:31 +0000

    Projects Showcase website link is https://projectnaga.ccbp.tech

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import {HeaderContainer, HeaderImage} from './styledComponents'
+
+const Header = () => (
+  <HeaderContainer>
+    <HeaderImage
+      src="https://assets.ccbp.in/frontend/react-js/projects-showcase/website-logo-img.png"
+      alt="website logo"
+      data-testid="loader"
+    />
+  </HeaderContainer>
+)
+
+export default Header
